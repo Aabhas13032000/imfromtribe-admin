bfd3f193cc52d7ecf1ced2abd6209b7c3b656491
Aabhas13032000 <choudharyaabhas13@gmail.com>
2022-03-14 21:08:32 +0530

    Sidebar design chenged mobile responsive issue solved

--- public/javascripts/sidebar.js
+++ public/javascripts/sidebar.js
@@ -15,9 +15,9 @@
     document.getElementById('loaded_page').setAttribute('src',`/user`);
     // First time bringing the box
     document.getElementsByClassName('list')[0].classList.add('selected');
-    var selected_list = document.getElementsByClassName('selected')[0];
-    document.getElementById('selected_card').style.top = `${selected_list.getBoundingClientRect().top}px`;
-    document.getElementById('selected_card').style.left = `${selected_list.getBoundingClientRect().left}px`;
+    // var selected_list = document.getElementsByClassName('selected')[0];
+    // document.getElementById('selected_card').style.top = `${selected_list.getBoundingClientRect().top}px`;
+    // document.getElementById('selected_card').style.left = `${selected_list.getBoundingClientRect().left}px`;
     // window.history.pushState("object or string", "Title", `/user`);
 // }
 
@@ -27,8 +27,8 @@ document.querySelectorAll('.list').forEach(item => {
         var inside_selected_list = document.getElementsByClassName('selected')[0];
         inside_selected_list.classList.remove('selected');
         item.classList.add('selected');
-        document.getElementById('selected_card').style.top = `${item.getBoundingClientRect().top}px`;
-        document.getElementById('selected_card').style.left = `${item.getBoundingClientRect().left}px`;
+        // document.getElementById('selected_card').style.top = `${item.getBoundingClientRect().top}px`;
+        // document.getElementById('selected_card').style.left = `${item.getBoundingClientRect().left}px`;
         var urlValue = item.getElementsByClassName('sidebar_text')[0].innerHTML.toLowerCase();
         document.getElementById('loaded_page').setAttribute('src',`/${urlValue}`);
         changeInitialPath(urlValue);
